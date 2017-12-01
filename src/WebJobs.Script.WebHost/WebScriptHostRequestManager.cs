6511640b64b5a7bc4f78bc3f269af62358d839d7
Mathew Charles <mathewc@microsoft.com>
2017-11-30 17:07:24 -0800

    Runtime Dynamic scaling prototype

--- src/WebJobs.Script.WebHost/WebScriptHostRequestManager.cs
+++ src/WebJobs.Script.WebHost/WebScriptHostRequestManager.cs
@@ -4,27 +4,80 @@
 using System;
 using System.Collections.ObjectModel;
 using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Azure.WebJobs.Script.Description;
 using Microsoft.Azure.WebJobs.Script.Diagnostics;
 using Microsoft.Azure.WebJobs.Script.Scale;
+using Microsoft.Azure.WebJobs.Script.Scaling;
 
 namespace Microsoft.Azure.WebJobs.Script.WebHost
 {
-    public class WebScriptHostRequestManager : HttpRequestManager
+    public class WebScriptHostRequestManager : HttpRequestManager, ILoadFactorProvider
     {
         private readonly HostPerformanceManager _performanceManager;
         private readonly IMetricsLogger _metricsLogger;
         private readonly int _performanceCheckPeriodSeconds;
+        private readonly object _syncLock = new object();
         private DateTime _lastPerformanceCheck;
         private bool _rejectAllRequests;
+        private int _incomingRequests;
+        private int _completedRequests;
 
         public WebScriptHostRequestManager(HttpExtensionConfiguration config, HostPerformanceManager performanceManager, IMetricsLogger metricsLogger, TraceWriter traceWriter, int performanceCheckPeriodSeconds = 15) : base(config, traceWriter)
         {
             _performanceManager = performanceManager;
             _metricsLogger = metricsLogger;
             _performanceCheckPeriodSeconds = performanceCheckPeriodSeconds;
+        }
+
+        public double GetLoadFactor()
+        {
+            var loadFactor = GetLoadFactor(_incomingRequests, _completedRequests);
+
+            lock (_syncLock)
+            {
+                // allow the periodic calls to define the measurement windows
+                _incomingRequests = 0;
+                _completedRequests = 0;
+            }
+
+            return loadFactor;
+        }
+
+        internal static double GetLoadFactor(int incomingRequests, int completedRequests)
+        {
+            double requestRatio = incomingRequests;
+            if (completedRequests > 0)
+            {
+                requestRatio = (double)incomingRequests / completedRequests;
+            }
+
+            // because a request ratio over 1 means incoming is greater than
+            // completed, we want to scale out if we're ever over 1
+            var loadFactor = requestRatio * (ScaleSettings.DefaultBusyWorkerLoadFactor / (double)100);
+
+            // the max load factor we want to return is 100
+            return Math.Min(loadFactor, 1);
+        }
+
+        public override async Task<HttpResponseMessage> ProcessRequestAsync(HttpRequestMessage request, Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> processRequestHandler, CancellationToken cancellationToken)
+        {
+            lock (_syncLock)
+            {
+                _incomingRequests++;
+            }
+
+            var response = await base.ProcessRequestAsync(request, processRequestHandler, cancellationToken);
+
+            lock (_syncLock)
+            {
+                _completedRequests++;
+            }
+
+            return response;
         }
 
         protected override bool RejectAllRequests()
